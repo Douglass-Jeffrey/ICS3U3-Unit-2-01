f8acdd20c20794c8cd30515cf111e88f04d881a3
Ubuntu <ubuntu@ip-172-31-19-198.ec2.internal>
2019-09-17 20:18:27 +0000

    initial commit

---
+++ Circle_program.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Douglass Jeffrey
+# Created on: September 2019
+# This is the circle program
+
+
+import math
+
+
+def main():
+    print("If a circle has a radius of 15mm:")
+    print("")
+    print("Area is {}mm^2".format(math.pi*15**2))
+    print("Perimeter is {}mm".format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
